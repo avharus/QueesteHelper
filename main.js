cfce183a8dae7ff011b6bbe5e138d492a5e720c0
Wouter Leeuw <avharus@gmail.com>
2017-08-08 17:48:05 +0200

    main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+$("document").ready(function() {
+  $("#roll").on('click', function() {
+    var n = document.getElementById('sterkte').value;
+    var result = 0;
+    $('#rollvalue').val($('#rollvalue').val() + "more text");
+  });
+});
