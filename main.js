68514f4a945ac2ef939c442a88c550601b8aa007
Wouter Leeuw <avharus@gmail.com>
2017-08-15 11:01:50 +0200

    Changed js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ $("document").ready(function() {
   $("#roll").on('click', function() {
     var n = document.getElementById('sterkte').value;
     var result = 0;
-    $('#rollvalue').val($('#rollvalue').val() + "more text");
+    document.getElementById('dobbelwaarde').value = result;
   });
 });
